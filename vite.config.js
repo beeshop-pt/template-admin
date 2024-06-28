a5b6b5999b785688d2bb67467f99999f9d8d770e
Marcos Ferreira <marcos_ferreira@outlook.pt>
2024-06-28 10:03:44 +0100

    lets see

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,21 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 
+  if (mode === 'production') {
+    return {
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+          '@helpers': resolve('../template-helpers')
+        }
+      },
+    }
+  }
+  
   return {
     plugins: [
       vue(),
@@ -18,5 +33,15 @@ export default defineConfig(({ mode }) => {
         '@helpers': resolve('../template-helpers')
       }
     },
+    build: {
+      outDir: '../../beeshop/public/assets/admin',
+      rollupOptions: {
+        output: {
+          entryFileNames: `[name].js`,
+          chunkFileNames: `[name].js`,
+          assetFileNames: `[name].[ext]`
+        },
+      },
+    }
   }
 })
