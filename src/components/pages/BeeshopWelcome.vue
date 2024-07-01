3792518f2b2a404ebac36eeca0eee33e3214c571
Guilherme Duarte <guilherme.duarte@setwin.pt>
2024-07-01 09:46:55 +0100

    Added welcome

--- src/components/pages/BeeshopWelcome.vue
+++ src/components/pages/BeeshopWelcome.vue
@@ -1,35 +1,29 @@
 <template>
-    <div class="container">
-        <div class="centered-div">
-            <div style="width: 500px" class="card">
-                <div class="card-content">
-                    <div class="field">
-                        <AppLogo/>
-                        <p style="text-align: center; font-size: 22px; font-weight: bold">Bem vindo ao Beeshop!</p>
+    <div class="centered-div">
+        <div style="width: 500px" class="card">
+            <div class="card-content">
+                <div class="field">
+                    <img style="display: block; width: 30%; margin-left: auto; margin-right: auto; margin-bottom: 10px" src="@/assets/logotypes/beeshop-abelha.svg">
+                    <p style="text-align: center; font-size: 22px; font-weight: bold">Bem vindo ao Beeshop!</p>
+                </div>
+                <div class="field">
+                    <p style="text-align: center">
+                        Antes de começar a utilizar todas as funcionalidades disponíveis, será necessário fazer algumas configurações:
+                        configurar o logotipo, o favicon, o email geral, o email de notificações e o limite máximo de ficheiros.
+                    </p>
+                </div>
+                <div style="margin: auto; width: 50%" class="field is-grouped">
+                    <div class="control">
+                        <button type="submit" id="welcome-later" class="button is-secondary">Mais tarde</button>
                     </div>
-                    <div class="field">
-                        <p style="text-align: center">
-                            Antes de começar a utilizar todas as funcionalidades do Beeshop, será necessário fazer algumas configurações:
-                            configurar o logotipo, o favicon, o email geral, o email de notificações e o limite máximo de ficheiros.
-                        </p>
-                    </div>
-                    <div style="margin: auto; width: 50%" class="field is-grouped">
-                        <div class="control">
-                            <button type="submit" id="welcome-later" class="button is-secondary">Mais tarde</button>
-                        </div>
-                        <div class="control">
-                            <button type="submit" id="welcome-start" class="button is-link">Começar</button>
-                        </div>
+                    <div class="control">
+                        <button type="submit" id="welcome-start" class="button is-link">Começar</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
-<script setup>
-
-</script>
 
 <style>
 
--- src/stories/Welcome.stories.js
+++ src/stories/Welcome.stories.js
@@ -1,11 +1,11 @@
-// import { fn } from '@storybook/test';
-import BeeshopWelcome from '@/components/pages/BeeshopWelcome.vue';
+import { fn } from '@storybook/test'
+import BeeshopWelcome from '@/components/pages/BeeshopWelcome.vue'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
   title: "Pages/Welcome",
   tags: ["autodocs"],
   component: BeeshopWelcome,
-};
+}
 
-export const Main = {};
+export const Main = {}
